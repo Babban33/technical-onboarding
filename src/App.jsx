9aec0f65eb134048d04318da09cbcb9fdaa80615
Chaitanya Wankhede <100457231+Babban33@users.noreply.github.com>
2023-09-05 10:54:35 +0000

    update: edited name and description, added images

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Chaitanya Wankhede",
+  title: "Software Developer & Data Scientist",
+  email: "Chaitanya.Wankhede@studentambassadors.com",
+  gitHub: "https:github.com/Babban33",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
